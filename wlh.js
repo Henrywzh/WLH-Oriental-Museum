83e015ada969411d8379fc69c2ddbd88bcb0c057
Henrywzh <97301969+Henrywzh@users.noreply.github.com>
2022-06-23 13:26:38 +0100

    Create wlh.js

---
+++ wlh.js
@@ -0,0 +1,23 @@
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+// Open the sidebar
+var mySidebar = document.getElementById("mySidebar");
+
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+  } else {
+    mySidebar.style.display = 'block';
+  }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
