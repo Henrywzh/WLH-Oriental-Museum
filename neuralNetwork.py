ead00b4b5e3742279e6fb23463c43df64ec9cc8e
Henrywzh <97301969+Henrywzh@users.noreply.github.com>
2022-07-14 15:23:39 +0100

    Create neuralNetwork.py

---
+++ neuralNetwork.py
@@ -0,0 +1,105 @@
+# ---- Digits ----
+""" Using Momentum update:
+Iteration: 0 / 1000
+14.587%
+Iteration: 100 / 1000
+83.735%
+Iteration: 200 / 1000
+88.638%
+Iteration: 300 / 1000
+90.445%
+Iteration: 400 / 1000
+91.337%
+Iteration: 500 / 1000
+91.992%
+Iteration: 600 / 1000
+92.392%
+Iteration: 700 / 1000
+92.648%
+Iteration: 800 / 1000
+92.880%
+Iteration: 900 / 1000
+93.128%
+Iteration: 1000 / 1000
+93.285%
+Accuracy: 93.040%
+"""
+
+""" Using Ordinary gradient descent:
+Iteration: 0 / 1000
+10.252%
+Iteration: 100 / 1000
+39.312%
+Iteration: 200 / 1000
+54.745%
+Iteration: 300 / 1000
+64.262%
+Iteration: 400 / 1000
+70.132%
+Iteration: 500 / 1000
+74.177%
+Iteration: 600 / 1000
+77.015%
+Iteration: 700 / 1000
+78.880%
+Iteration: 800 / 1000
+80.385%
+Iteration: 900 / 1000
+81.520%
+Iteration: 1000 / 1000
+82.343%
+Accuracy: 83.410%
+"""
+
+# ---- Images ----
+""" Using Momentum update:
+Iteration: 0 / 1000
+5.232%
+Iteration: 100 / 1000
+72.107%
+Iteration: 200 / 1000
+76.442%
+Iteration: 300 / 1000
+78.997%
+Iteration: 400 / 1000
+80.787%
+Iteration: 500 / 1000
+81.865%
+Iteration: 600 / 1000
+82.578%
+Iteration: 700 / 1000
+83.172%
+Iteration: 800 / 1000
+83.585%
+Iteration: 900 / 1000
+84.005%
+Iteration: 1000 / 1000
+84.330%
+Accuracy: 82.880%
+"""
+
+""" Using Ordinary gradient descent:
+Iteration: 0 / 1000
+11.260%
+Iteration: 100 / 1000
+53.437%
+Iteration: 200 / 1000
+62.270%
+Iteration: 300 / 1000
+65.912%
+Iteration: 400 / 1000
+68.155%
+Iteration: 500 / 1000
+69.597%
+Iteration: 600 / 1000
+70.880%
+Iteration: 700 / 1000
+71.923%
+Iteration: 800 / 1000
+72.857%
+Iteration: 900 / 1000
+73.595%
+Iteration: 1000 / 1000
+74.352%
+Accuracy: 73.190%
+"""
