eb80fd362e1cc4a7cf5fea793b943505992cc157
Henrywzh <97301969+Henrywzh@users.noreply.github.com>
2022-07-27 12:59:48 +0100

    Update 1StockPricePredictionResults.py

--- 1StockPricePredictionResults.py
+++ 1StockPricePredictionResults.py
@@ -14,3 +14,5 @@ Stock Price Model:
 y = 0.21045564471415734 + -0.9537579670957308x1 + 3.044632906544958x2 + -1.0937454729432503x3+ 0.004973243198474358x4 + -0.3140980678969668x5
 R-squared score: 0.9402591205648176
 """
+# This is the link for the LSTM Model
+# https://medium.com/the-handbook-of-coding-in-finance/stock-prices-prediction-using-long-short-term-memory-lstm-model-in-python-734dd1ed6827
