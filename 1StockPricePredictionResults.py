e7ecb860547ea690d5a694c8bee69739ec23da6a
Henrywzh <97301969+Henrywzh@users.noreply.github.com>
2022-09-16 21:38:45 +0100

    Update 1StockPricePredictionResults.py

--- 1StockPricePredictionResults.py
+++ 1StockPricePredictionResults.py
@@ -1,4 +1,5 @@
-""" Stock Price Model Comparison, clearly my model is slightly more accurate than the online LSTM machine learning model.
+""" 
+Stock Price Model Comparison, clearly my model is slightly more accurate than the online LSTM machine learning model.
 Epoch 1/4
 494/494 [==============================] - 12s 20ms/step - loss: 43.2724
 Epoch 2/4
@@ -8,9 +9,10 @@ Epoch 3/4
 Epoch 4/4
 494/494 [==============================] - 10s 20ms/step - loss: 0.9299
 5/5 [==============================] - 1s 13ms/step
-LSTM Model:
+Online LSTM Model:
 R-squared score:  0.92210114
-Stock Price Model:
+
+My Multiple Regression Stock Price Model:
 y = 0.21045564471415734 + -0.9537579670957308x1 + 3.044632906544958x2 + -1.0937454729432503x3+ 0.004973243198474358x4 + -0.3140980678969668x5
 R-squared score: 0.9402591205648176
 """
