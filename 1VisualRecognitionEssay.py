ee318d8b71329f7108996e5c44f476e74aac5e0e
Henrywzh <97301969+Henrywzh@users.noreply.github.com>
2022-09-18 19:17:47 +0100

    Update 1VisualRecognitionEssay.py

--- 1VisualRecognitionEssay.py
+++ 1VisualRecognitionEssay.py
@@ -1,3 +1,3 @@
-"""
+""" My essays are not formal, I wrote them simply to deepen my understanding of the theory behind the projects. 
 https://drive.google.com/file/d/14N0l9NBm0gkjU6nBoB9lrJCy6z_nj1Km/view?usp=sharing
 """
