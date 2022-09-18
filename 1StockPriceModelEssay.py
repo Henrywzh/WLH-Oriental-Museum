ea462ed38012822875a66564379a625426d372e4
Henrywzh <97301969+Henrywzh@users.noreply.github.com>
2022-09-18 19:17:05 +0100

    Update 1StockPriceModelEssay.py

--- 1StockPriceModelEssay.py
+++ 1StockPriceModelEssay.py
@@ -1,3 +1,3 @@
-"""
+""" My essays are not formal, I wrote them simply to deepen my understanding of the theory behind the projects. 
 https://drive.google.com/file/d/1PD0JrLrspoi0kH97lTbvHIo7u6HOek9N/view?usp=sharing
 """
