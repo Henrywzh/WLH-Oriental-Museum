570b78feccb009fd3c728626a5b5a0c0333f86aa
Henrywzh <97301969+Henrywzh@users.noreply.github.com>
2022-09-18 19:17:30 +0100

    Update 1StockPricePredictionEssay.py

--- 1StockPricePredictionEssay.py
+++ 1StockPricePredictionEssay.py
@@ -1,3 +1,3 @@
-"""
+""" My essays are not formal, I wrote them simply to deepen my understanding of the theory behind the projects. 
 https://drive.google.com/file/d/1Dr3HRqegWXd4tKPyLlToTSSVJTI6uwXl/view?usp=sharing
 """
